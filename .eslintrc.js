488623dffe9f080facc8fd22dd757db65a9a25b7
Daniel Kiesshau <danielkiesshau@outlook.com>
2021-07-05 20:10:49 -0300

    chore(tslint)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['@react-native-community', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': ['error'],
+    '@typescript-eslint/ban-types': 'off',
+  },
 };
