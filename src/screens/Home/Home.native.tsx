225b931a054af9db54f900ce79e0e9c2023f11dc
Daniel Kiesshau <danielkiesshau@outlook.com>
2021-08-05 20:20:58 -0300

    refactor(home): flatlist column style

--- src/screens/Home/Home.native.tsx
+++ src/screens/Home/Home.native.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from 'react';
 import { View, Text } from 'react-native';
-import { RectButton } from 'react-native-gesture-handler';
+import { FlatList, RectButton } from 'react-native-gesture-handler';
 
 import animationChallenges from '@mocks/animationChallenges';
 import ScreensInterfaces from '@routes/ScreensInterfaces';
@@ -9,7 +9,7 @@ import Styles from './Home.styles';
 
 const HomeNative: React.FC<ScreensInterfaces> = ({ navigation }) => {
   const renderItem = useCallback(
-    item => {
+    ({ item }) => {
       const Icon = item.icon;
 
       const handleChallengePress = (): void => {
@@ -31,14 +31,16 @@ const HomeNative: React.FC<ScreensInterfaces> = ({ navigation }) => {
     [navigation],
   );
 
-  const renderChallenges = useCallback(
-    () => animationChallenges.map(renderItem),
-    [renderItem],
+  return (
+    <View style={Styles.containerPage}>
+      <FlatList
+        data={animationChallenges}
+        renderItem={renderItem}
+        numColumns={3}
+        columnWrapperStyle={Styles.columns}
+      />
+    </View>
   );
-
-  const challenges = renderChallenges();
-
-  return <View style={Styles.containerPage}>{challenges}</View>;
 };
 
 export default HomeNative;
